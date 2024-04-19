10471d236e714104eab4c3b9a1bfe7ce335d1855
kevin <kevnsc18@gmail.com>
2024-04-18 21:06:50 -0500

    rutas listas

---
+++ routers/user.routes.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+
+const rutas=express.Router();
+
+rutas.get('/getall');
+rutas.get('/getall:id');
+rutas.post('/registrar');
+rutas.put('/actualizar:id');
+rutas.delete('/borras:id');
+
+export default rutas;
