0f527bff5f0441e649147f7839e2384787593f1c
santyxxo <betancourtlopezjorge@gmail.com>
2024-04-18 21:39:14 -0500

    rutas modificadas

--- routers/user.routes.js
+++ routers/user.routes.js
@@ -3,7 +3,7 @@ import express from "express";
 
 const rutas=express.Router();
 
-rutas.get('/getall');
+rutas.get('/obtenertodo');
 rutas.get('/getall:id');
 rutas.post('/registrar');
 rutas.put('/actualizar:id');
